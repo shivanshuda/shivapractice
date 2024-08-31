679475c1d18bc314ed3be09df59f67ce6c29cc48
shivanshuda <124139345+shivanshuda@users.noreply.github.com>
2024-08-31 10:27:04 +0000

    data types

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict";
+//alert(3+3)
+console.log(3+3);
+let name = "shiva"// string can be in single quates
+let age = 18//number(2^53)
+let isloggedin = false // boolean
+//null stand alone value
+//undefined ==> 
+    let state;
+//symbol ==> unique 
+
+//object 
+console.log(typeof "hitesh");
+console.log(typeof null);// gives object
+console.log(typeof undefined);// undefined
